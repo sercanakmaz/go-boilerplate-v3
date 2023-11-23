64412042ff21260876550bbe899f8709ed301e9f
ergin.eker <ergin.eker@hepsiburada.com>
2023-11-23 16:36:48 +0300

    added delete elastic index consumer

--- contexts/product/elastic-product-consumer/elastic-product-consumer.go
+++ contexts/product/elastic-product-consumer/elastic-product-consumer.go
@@ -5,10 +5,13 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"github.com/sercanakmaz/go-boilerplate-v3/events/product/products"
 	logger "github.com/sercanakmaz/go-boilerplate-v3/pkg/log"
 	"github.com/sercanakmaz/go-boilerplate-v3/pkg/rabbitmqv1"
 )
+
+var indexName = "product_ddd_qa"
 
 type ElasticProductConsumer struct {
 	MessageBus *rabbitmqv1.Client
@@ -28,6 +31,10 @@ func (c *ElasticProductConsumer) Construct() {
 	c.MessageBus.AddConsumer("HG.Elastic.Product.Create").
 		SubscriberExchange("*", rabbitmqv1.Topic, "HG.Integration.Product:Created").
 		HandleConsumer(c.createElasticProduct())
+
+	c.MessageBus.AddConsumer("HG.Elastic.Product.Delete").
+		SubscriberExchange("*", rabbitmqv1.Topic, "HG.Integration.Product:Deleted").
+		HandleConsumer(c.deleteElasticProduct())
 }
 
 func (c *ElasticProductConsumer) createElasticProduct() func(message rabbitmqv1.Message) error {
@@ -46,12 +53,48 @@ func (c *ElasticProductConsumer) createElasticProduct() func(message rabbitmqv1.
 
 		data, _ := json.Marshal(eventMessage)
 
-		_, err := c.Client.Index("product_ddd_qa", bytes.NewReader(data))
+		indexRequest := esapi.IndexRequest{
+			Index:      indexName,
+			DocumentID: eventMessage.Id,
+			Body:       bytes.NewReader(data),
+			Refresh:    "true",
+		}
+
+		_, err := indexRequest.Do(context.Background(), c.Client)
 		if err != nil {
 			return err
 		}
 
 		c.Logger.Info(ctx, "consumer elastic finish")
+
+		return nil
+	}
+}
+
+func (c *ElasticProductConsumer) deleteElasticProduct() func(message rabbitmqv1.Message) error {
+	return func(message rabbitmqv1.Message) error {
+		var eventMessage products.Created
+
+		ctx := c.Logger.WithCorrelationId(context.Background(), message.GetCorrelationId())
+
+		if err := json.Unmarshal(message.Payload, &eventMessage); err != nil {
+			return err
+		}
+
+		c.Logger.Info(ctx, "consumer delete elastic start")
+		c.Logger.Info(ctx, eventMessage.Sku)
+
+		deleteRequest := esapi.DeleteRequest{
+			Index:      indexName,
+			DocumentID: eventMessage.Id,
+		}
+
+		_, err := deleteRequest.Do(context.Background(), c.Client)
+		if err != nil {
+			return err
+		}
+
+		c.Logger.Info(ctx, "consumer delete elastic finish")
 
 		return nil
 	}
